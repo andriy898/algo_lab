5cda0ff1d3b17b5f0dce7d13fafd849d7803226a
andriy898 <105068614+andriy898@users.noreply.github.com>
2024-10-19 22:33:43 +0300

    lab3.py

--- lab3.py
+++ lab3.py
@@ -2,7 +2,6 @@ def find_dominator(arr):
     if not arr:
         return -1
     
-    # Крок 1: Знаходження кандидата на домінатора
     candidate = None
     count = 0
     
@@ -11,7 +10,6 @@ def find_dominator(arr):
             candidate = num
         count += (1 if num == candidate else -1)
     
-    # Крок 2: Перевірка, чи кандидат дійсно є домінатором
     if candidate is not None:
         occurrence = arr.count(candidate)
         if occurrence > len(arr) // 2:
@@ -19,7 +17,6 @@ def find_dominator(arr):
     
     return -1
 
-# Приклад використання
 example = [3,4,3,2,3,1,3,3]
-print(find_dominator(example))  # Виведе: 3
+print(find_dominator(example))  
 
